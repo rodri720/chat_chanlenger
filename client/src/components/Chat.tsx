c53716ff8adedae8fd31b690da88bd0af439c3d1
rodri720 <tomiyrami@gamil.com>
2023-08-14 21:33:54 -0300

    repo listo

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Chat.css';
-//* librería que proporciona la funcionalidad de WebSockets para establecer una conexión en tiempo real con el servidor.
-import io from 'socket.io-client'; 
+import io from 'socket.io-client';
 
 interface Message {
   content: string;
@@ -11,66 +10,55 @@ interface Message {
   fecha: Date;
 }
 
-//TODO Este componente va a encargarse de mostrar la lista de mensajes.
 const Chat = () => {
-  const [chatMessages, setChatMessages] = useState<Message[]>([])
+  const [chatMessages, setChatMessages] = useState<Message[]>([]);
 
-  function callMessages():void {
+  function callMessages(): void {
     axios.get<Message[]>('http://localhost:3002/messages')
       .then((response) => {
-        console.log('Message loaded successfully!', response.data);
-        setChatMessages(response.data)
+        console.log('Messages loaded successfully!', response.data);
+        setChatMessages(response.data);
       })
       .catch(error => {
-        console.error('Error loading message:', error);
-        // Aquí puedes manejar el error de envío de mensaje
-      })
+        console.error('Error loading messages:', error);
+        // Handle the error
+      });
   }
 
   function formatDate(date: Date): string {
-  
     const dateString = date?.toString();
-    const day = `${new Date(date).getDate()}/${new Date(date).getMonth()+1}`
-    const hour = `${new Date(date).getHours()}:${dateString.substring(14, 16)}`
-
+    const day = `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}`;
+    const hour = `${new Date(date).getHours()}:${dateString.substring(14, 16)}`;
     return ` ${day} at ${hour}`;
   }
 
-const socket = io('http://localhost:3002');   //* Se crea una instancia de Socket.io utilizando la URL del servidor
+  const socket = io('http://localhost:3002');
 
-socket.on('connect', ()=> console.log('Tenemos comunicación bidireccional en tiempo real'))
+  socket.on('connect', () => console.log('Bidirectional real-time communication established'));
 
-  socket.on('update', (data: Message) => {      //* Este evento se activa cuando se recibe una actualización de mensajes en tiempo real desde el server
-    callMessages() //* Llamamos a la nueva lista de mensajes
+  socket.on('update', (data: Message) => {
+    // Here 'data' is the received message, and you can handle it as needed.
+    console.log('Received update:', data);
+    callMessages(); // Update the message list with new data
   });
 
   useEffect(() => {
-    callMessages()
+    callMessages();
   }, []);
-
-
-
 
   return (
     <div className='messagesContainer'>
-      {
-        chatMessages?.map(message => (
-          <div className='chatBubble' key={message.fecha.toString()}>
-            <p className='date'>{formatDate(message.fecha)}</p>
-            <p className='interlocutors'><span>From:</span> {message.sender}</p>
-            <p className='interlocutors'><span>To:</span> {message.recipient}</p>
-            <br />
-            <p className='message'>{message.content}</p>
-
-
-
-
-          </div>
-        ))
-      }
+      {chatMessages?.map(message => (
+        <div className='chatBubble' key={message.fecha.toString()}>
+          <p className='date'>{formatDate(message.fecha)}</p>
+          <p className='interlocutors'><span>From:</span> {message.sender}</p>
+          <p className='interlocutors'><span>To:</span> {message.recipient}</p>
+          <br />
+          <p className='message'>{message.content}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Chat
-
+export default Chat;
